efd73f03d90ae83aaa965e90a154c20e57cbb311
Seyed Amirhossein <amir.hossein.1384.musavi@gmail.com>
2025-04-17 11:38:06 +0330

    Solving Bugs

--- WebScraper_Alibaba.py
+++ WebScraper_Alibaba.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 # URL and Setting Headers
-url = r"https://www.alibaba.ir/accommodation/search?destination=province-gilan&title=%D8%A7%D8%B3%D8%AA%D8%A7%D9%86+%DA%AF%DB%8C%D9%84%D8%A7%D9%86&checkin=1404-01-30&checkout=1404-01-31&count=5&accommodationTypes=villa"
+url = r"https://www.alibaba.ir/accommodation/search?destination=province-gilan&title=%D8%A7%D8%B3%D8%AA%D8%A7%D9%86+%DA%AF%DB%8C%D9%84%D8%A7%D9%86&checkin=1404-02-01&checkout=1404-02-02&count=5&accommodationTypes=villa"
 
 options = Options()
 # options.add_argument("--headless")
@@ -13,11 +13,14 @@ options.add_argument("--disable-gpu")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
 driver = webdriver.Chrome(options=options)
 driver.get(url)
-for _ in range(50):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # Scrolling Down To Load More ADs
+scroll = int(input("Scroll Number: "))
+sleep_time = int(input("Sleep Time: "))
+for _ in range(scroll):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(0.5)
     #waiting to load the HTML file
-print("waiting for 20 seconds... \n Scrolling Down to Load More ADs")
-time.sleep(20)
+print(f"waiting for {sleep_time} seconds... \n Scrolling Down to Load More ADs")
+time.sleep(sleep_time)
     
 html = driver.page_source
 # print("Waiting for villas to load...")
@@ -42,6 +45,6 @@ for villa in villas:
         f.write(f"Villa Name is: {villa_name}\n")
         f.write(f"Villa Price for Every Night is: {villa_price}\n")
         f.write(f"Villa Basic Information is: {villa_place_info}\n")
-        f.write(50*"==="+ "\n\n")
+        f.write(50*"=="+ "\n\n")
 print(f"{count} Numbers of ADs Scraped.")
 driver.quit()

ed1a5bc0531a8676fd52eebdee4200745b2b72ea
Seyed Amirhossein <amir.hossein.1384.musavi@gmail.com>
2025-04-18 15:19:41 +0330

    Update CodeExtractor.py

--- CodeExtractor.py
+++ CodeExtractor.py
@@ -1,12 +1,15 @@
 import json
 
-with open("Search.json", "r", encoding="utf-8") as file:
+with open("result_from_api.json", "r", encoding="utf-8") as file:
     data = json.load(file)
 
 all_codes = []
-for item in data["result"]["items"]:
-    if "code" in item:
-        all_codes.append(item["code"])
 
-print("All Codes: ")
+for page in data:
+    items = page.get("result", {}).get("items", [])
+    for item in items:
+        if "code" in item:
+            all_codes.append(item["code"])
+
+print("All Codes:")
 print(all_codes)

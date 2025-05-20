8e0b1972ce80a58708a9853e23e23024fd5f31a7
Seyed Amirhossein Mousavi <amir.hossein.1384.musavi@gmail.com>
2025-05-20 18:57:01 +0330

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -2,10 +2,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # خواندن فایل‌ها
-with open("all_villa_data_otaghak.txt", "r", encoding="utf-8") as f1:
+with open("all_otaghak_villa_data.txt", "r", encoding="utf-8") as f1:
     ads1 = f1.read().split("==================================================")
 
-with open("alldata_alibaba_links.txt", "r", encoding="utf-8") as f2:
+with open("alldata_Alibaba_links.txt", "r", encoding="utf-8") as f2:
     ads2 = f2.read().split("====================================================================================================")
 
 # پیش‌پردازش  و حذف فضای خالی و تمیزکاری
@@ -33,4 +33,6 @@ for i in range(len(ads1)):
 
 # چاپ نمونه نتیجه
 for match in matches:
-    print(f"Matched ({match[2]}%):\n- {match[0][:60]}...\n- {match[1][:60]}...\n")
+    with open(r"similarity.txt", "a", encoding="utf-8") as f:
+        f.write(f"Matched ({match[2]}%):\n- {match[0][:60]}...\n- {match[1][:60]}...\n")
+        f.write(50*"=="+"\n")

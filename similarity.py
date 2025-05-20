c306e065597cae026b7c80224f9e9d66c2cc3415
Seyed Amirhossein Mousavi <amir.hossein.1384.musavi@gmail.com>
2025-05-20 18:58:12 +0330

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -1,27 +1,26 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# خواندن فایل‌ها
+
 with open("all_otaghak_villa_data.txt", "r", encoding="utf-8") as f1:
     ads1 = f1.read().split("==================================================")
 
 with open("alldata_Alibaba_links.txt", "r", encoding="utf-8") as f2:
     ads2 = f2.read().split("====================================================================================================")
 
-# پیش‌پردازش  و حذف فضای خالی و تمیزکاری
+
 ads1 = [ad.strip() for ad in ads1 if ad.strip()]
 ads2 = [ad.strip() for ad in ads2 if ad.strip()]
 
-# برداری‌سازی
+
 vectorizer = TfidfVectorizer()
 tfidf = vectorizer.fit_transform(ads1 + ads2)
 tfidf1 = tfidf[:len(ads1)]
 tfidf2 = tfidf[len(ads1):]
 
-# محاسبه مشابهت
 similarity_matrix = cosine_similarity(tfidf1, tfidf2)
 
-# آستانه مشابهت  
+
 threshold = 10
 matches = []
 
@@ -31,7 +30,7 @@ for i in range(len(ads1)):
         if score >= threshold:
             matches.append((ads1[i], ads2[j], round(score, 2)))
 
-# چاپ نمونه نتیجه
+
 for match in matches:
     with open(r"similarity.txt", "a", encoding="utf-8") as f:
         f.write(f"Matched ({match[2]}%):\n- {match[0][:60]}...\n- {match[1][:60]}...\n")
